19f5cc9b8854a2ced8008e60ecae8bafce0f27f9
Ramkumar Velmurugan <vrk1990@outlook.com>
2018-12-04 07:21:20 -0400

    Kotlin Refactor

---
+++ app/src/main/java/com/dija/skynet/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dija.skynet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dija.skynet.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/dija/skynet/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.dija.skynet.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
